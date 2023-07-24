b784e7fe73bccd541b80ac2fbe9282e6c9ff6d68
t-furuta <takahisa.furuta@sparty.jp>
2023-07-24 10:54:26 +0900

    wip

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { MantineProvider } from "@mantine/core";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        /** Put your mantine theme override here */
+        colorScheme: "light",
+        fontFamily: "Verdana, sans-serif",
+      }}
+    >
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 }
